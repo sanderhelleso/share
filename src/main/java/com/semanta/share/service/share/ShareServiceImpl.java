df6ac30a1c6435ec1447cb9cbdd79b1ec1f62a24
Sanderhelleso <sanderhelleso@hotmail.com>
2019-08-19 18:11:12 +0200

    make_share_service: added method to create file in releative path to project

--- src/main/java/com/semanta/share/service/share/ShareServiceImpl.java
+++ src/main/java/com/semanta/share/service/share/ShareServiceImpl.java
@@ -1,20 +1,27 @@
 package com.semanta.share.service.share;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShareServiceImpl implements ShareService {
+    private final String SHARE_DIR = "tmp-dirs";
+    private final String WRk_DIR = Paths.get(".").toAbsolutePath().normalize().toString();
 
     public String makeTmpDir() {
-        String dirName = this.genNounce();
-        new File("/" + dirName).mkdirs();
+        String dirName = concatDirs(this.genNounce());
+        new File(dirName).mkdirs();
         return dirName;
     }
 
     private String genNounce() {
         return UUID.randomUUID().toString();
+    }
+
+    private String concatDirs(String s) {
+        return WRk_DIR + File.separatorChar + SHARE_DIR + File.separatorChar + s;
     }
 }
