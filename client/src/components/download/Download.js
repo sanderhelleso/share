9bc45c8872da2bd09349fba76045a324f3110ba8
Sanderhelleso <sanderhelleso@hotmail.com>
2019-08-24 20:42:17 +0200

    create_download_component: added check for valid code

--- client/src/components/download/Download.js
+++ client/src/components/download/Download.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import _fetch from '../../util/_fetch';
+import { withRouter } from 'react-router-dom';
 
-const Download = () => {
-	return <h1>Download</h1>;
+const baseUrl = 'http://localhost:4000/share/retrieve?dirID=';
+
+const Download = ({ match: { params } }) => {
+	const [ data, setData ] = useState();
+
+	useEffect(() => {
+		fetchShare();
+	}, []);
+
+	const fetchShare = async () => {
+		const { shareCode } = params;
+
+		try {
+			const response = await _fetch(`${baseUrl}${shareCode}`);
+			const data = await response.json();
+			setData(data);
+		} catch (error) {
+			setData(false);
+		}
+	};
+
+	const render = () => {
+		if (data) {
+			return <h1>Valid</h1>;
+		}
+
+		if (data === false) {
+			return <h1>Invalid code</h1>;
+		}
+
+		return <h1>Loading...</h1>;
+	};
+
+	return render();
 };
 
-export default Download;
+export default withRouter(Download);
